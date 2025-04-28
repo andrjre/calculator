3c7713e8013aaec0511b97a06a830d3a789d4d22
andrjre <andrjre@gmail.com>
2025-04-28 09:58:15 +0100

    Click functionality.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let button = document.getElementById("button")
+let btns = document.querySelectorAll(".btn")
+let equals = document.querySelector("#equals")
+let screen = document.querySelector("#screen")
+let clear = document.querySelector("#clear")
 
-button.onclick = function(){
-    console.log("button press")
-}
+btns.forEach(function(btns){
+    btns.addEventListener("click", function(){
+        console.log(btns.id);
+        let inputs = [btns.id]   
+        console.log(inputs)
+        screen.textContent = btns.id
+    })
+})
+
+equals.addEventListener("click", function(){
+    console.log("answer")
+    screen.textContent = "answer"
+   
+})
+
+clear.addEventListener("click", function(){
+screen.textContent = ""
+console.log("screen cleared")
+})
