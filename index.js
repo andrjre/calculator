fb96e266a4e568aac7e10216e7d6cf823daa184b
andrjre <andrjre@gmail.com>
2025-05-05 21:14:20 +0100

    Styling.

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ let clear = document.querySelector("#clear")
 
 btns.forEach(function(btns){
     btns.addEventListener("click", function(){
-        console.log(btns.id);
-        let inputs = [btns.id]   
+        let inputs = [btns.id]
         console.log(inputs)
         screen.textContent = btns.id
     })
 })
 
 equals.addEventListener("click", function(){
-    console.log("answer")
     screen.textContent = "answer"
-   
 })
 
 clear.addEventListener("click", function(){
 screen.textContent = ""
 console.log("screen cleared")
 })
+
